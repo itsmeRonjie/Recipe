6f6f7234f73e621611effaf06fb3ab43a094082e
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-22 10:33:12 +0800

    init

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Ronjie Diafante Man-on on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

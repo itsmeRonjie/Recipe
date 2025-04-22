839831e70a92bc02a79ec2bd4a21193810b57ce5
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-04-22 11:18:01 +0800

    - restructure to MVVM pattern - added RecipeModel

--- Recipe/Model/RecipeModel.swift
+++ Recipe/Model/RecipeModel.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct RecipeModel: Identifiable {
+    let id = UUID()
+    let name: String
+    let caloriesPer100Grams: Int
+    let recipe: String
+    let recipeImage: String
+    let recipeURL: String
+}
